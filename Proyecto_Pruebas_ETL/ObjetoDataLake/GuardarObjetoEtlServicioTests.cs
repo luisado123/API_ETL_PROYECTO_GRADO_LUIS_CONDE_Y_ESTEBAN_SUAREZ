ba501c445c782aeaf1e585a0a7c7214eeb4fe79b
luisado123 <dro783249@gmail.com>
2024-02-22 09:52:51 -0500

    pruebas unitarias servicios data lake

---
+++ Proyecto_Pruebas_ETL/ObjetoDataLake/GuardarObjetoEtlServicioTests.cs
@@ -0,0 +1,72 @@
+﻿using Xunit;
+using NSubstitute;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UTS.Etl.LuisConde.EstebanSuarez.Infraestructura.Adaptadores;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.Puertos;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.Entidades;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.servicios.ArchivoXlsx;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.Constantes;
+using NSubstitute.ExceptionExtensions;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.Excepciones;
+using FluentAssertions;
+namespace UTS.Etl.LuisConde.EstebanSuarez.Dominio.Tests
+{
+    public class GuardarObjetoEtlServicioTests
+    {
+        private readonly IDataLakeRepositorio _dataLakeRepositorioMock;
+
+        public GuardarObjetoEtlServicioTests()
+        {
+            _dataLakeRepositorioMock = Substitute.For<IDataLakeRepositorio>();
+        }
+
+        [Fact]
+        public async Task GuardarRawDataAsyncServicio_GuardaUnoEnDataLakeRepositorio()
+        {
+            // Arrange
+            var objetoDataLake = new Entidades.ObjetoDataLake
+            {
+                Datos = new List<Dictionary<string, object>>(),
+                DepartamentoOrigen = "Departamento"
+            };
+
+            var servicio = new GuardarObjetoEtlServicio(_dataLakeRepositorioMock);
+            _dataLakeRepositorioMock.GuardarUno(Arg.Any<string>()).Returns(Task.FromResult<IActionResult>(new OkObjectResult(new { Message = MensajesExitosos.CargaEnDataWareHouseExitosa })));
+
+            // Act
+            var resultado =await servicio.GuardarRawDataAsync(objetoDataLake);
+
+            // Assert
+            await _dataLakeRepositorioMock.Received(1).GuardarUno(Arg.Any<string>());
+            var okResult = resultado as OkObjectResult;
+            okResult.Should().NotBeNull();
+            var message = okResult.Value.GetType().GetProperty("Message")?.GetValue(okResult.Value)?.ToString();
+            message.Should().Contain(MensajesExitosos.CargaEnDataWareHouseExitosa);
+
+        }
+
+
+        [Fact]
+        public async Task GuardarUno_DeberiaLanzarExcepcionCuandoFallaLaInsercion()
+        {
+            // Arrange
+            var objetoDataLake = new Entidades.ObjetoDataLake
+            {
+                Datos = new List<Dictionary<string, object>>(),
+                DepartamentoOrigen = "Departamento"
+            };
+
+            var error = "Error interno";
+
+            var servicio = new GuardarObjetoEtlServicio(_dataLakeRepositorioMock);
+            _dataLakeRepositorioMock.GuardarUno(Arg.Any<string>()).Throws(new GuardadoEnDataLakeFallidoExcepcion($"{MensajesExcepciones.ErrorAlGuardar}: {error}"));
+
+            // Act y Assert
+            await Assert.ThrowsAsync<GuardadoEnDataLakeFallidoExcepcion>(() => servicio.GuardarRawDataAsync(objetoDataLake));
+
+        }
+
+    }
+
+}
---
+++ Proyecto_Pruebas_ETL/ObjetoDataLake/ObtenerDatosPorDepartamentoServicioTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.Constantes;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.Entidades;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.Excepciones;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.Puertos;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.servicios.ArchivoXlsx;
+using UTS.Etl.LuisConde.EstebanSuarez.Dominio.Servicios.ObjetoDataLake;
+using Xunit;
+
+namespace UTS.Etl.LuisConde.EstebanSuarez.Dominio.Tests.ObjetoDataLake
+{
+    public class ObtenerDatosPorDepartamentoServicioTests
+    {
+        private readonly IDataLakeRepositorio _dataLakeRepositorioMock;
+        public ObtenerDatosPorDepartamentoServicioTests()
+        {
+            _dataLakeRepositorioMock = Substitute.For<IDataLakeRepositorio>();
+        }
+
+        [Fact]
+        public async Task ObtenerPorDepartamentoAsync_DeberiaDevolverListaDeRespuestaConsultaPorDepartamento()
+        {
+            // Arrange
+            var idDepartamento = "1";
+            var servicio = new ObtenerDatosPorDepartamentoServicio(_dataLakeRepositorioMock);
+            var listaRespuestas = new List<RespuestaConsultaPorDepartamento>();
+
+            _dataLakeRepositorioMock.ObtenerPorCampo(idDepartamento).Returns(Task.FromResult(listaRespuestas));
+
+            // Act
+            var resultado = await servicio.ObtenerPorDepartamentoAsync(idDepartamento);
+
+            // Assert
+            Assert.IsType<List<RespuestaConsultaPorDepartamento>>(resultado);
+        }
+
+        [Fact]
+        public async Task ObtenerPorDepartamentoAsync_DeberiaLanzarExcepcionCuandoNoHayRegistros()
+        {
+            // Arrange
+            var idDepartamento = "1";
+            var servicio = new ObtenerDatosPorDepartamentoServicio(_dataLakeRepositorioMock);
+            var listaRespuestas = new List<RespuestaConsultaPorDepartamento>();
+
+            _dataLakeRepositorioMock.ObtenerPorCampo(Arg.Any<string>()).Throws(new ObtensionPorDepartamentoException(MensajesExcepciones.NoHayRegistros));
+
+            // Act y Assert
+            await Assert.ThrowsAsync<ObtensionPorDepartamentoException>(() => servicio.ObtenerPorDepartamentoAsync(idDepartamento));
+        }
+
+
+
+    }
+}
